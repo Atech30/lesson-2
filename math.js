b48ae0eb949f166db9714557cee85f64272dd970
Abdul bobsonkabia kabia <a_kabia@yahoo.com>
2019-01-12 15:29:25 -0500

    learning math in javascript

---
+++ math.js
@@ -0,0 +1,13 @@
+let num1=10;
+let num2=22;
+console.log( num1 + num2);
+
+num1=100;
+
+console.log(`${num1}`)
+console.log( num1-num2);
+console.log (num1*num2);
+num1/num2;
+4%2;
+5%2; 
+num1 / (25 + 10);
