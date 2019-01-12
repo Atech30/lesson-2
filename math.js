5684bfd09fd63baa81f5d46737d78af51a4b0571
Abdul bobsonkabia kabia <a_kabia@yahoo.com>
2019-01-12 15:53:15 -0500

    completed first quiz

--- math.js
+++ math.js
@@ -11,3 +11,16 @@ num1/num2;
 4%2;
 5%2; 
 num1 / (25 + 10);
+
+// Create a variable called dayOfWeek and assign it to today
+const dayOfWeek="saturday";
+// Create a variable called myBirthday and assign it to the string value of your birthday example May 5, 2019
+const myBirthday =" july 26,1996";
+
+// Print to the screen the message [I am a developer!!!!]
+console.log ("i am a develper");
+// Create two variables (number1: 55, number2:1110) and divide them 
+const number1=55
+const number2=1110
+console.log( number2/number1)
+
